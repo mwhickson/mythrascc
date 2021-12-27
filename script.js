dc4a932fe9911cdea2346ad01cbb739afcb2ba7c
Matthew Hickson <matthewhickson@outlook.com>
2021-12-27 16:57:35 -0500

    adds secondary attribute calculation

--- script.js
+++ script.js
@@ -24,11 +24,53 @@ const attribute_keys = {
     }
 }
 
+const in_numeric_range = (value, start, end) => ((value >= start) && (value <= end))
 const make_pool = die_sizes => [].concat(die_sizes)
 const roll_die = sides => Math.floor(Math.random() * sides) + 1
 const roll_pool = pool => pool.map(roll_die)
 const sum_pool = pool => pool.reduce((a, b) => a + b)
 const roll_attribute = (pool, modifier) => sum_pool(roll_pool(pool)) + (modifier ? modifier : 0)
+
+const calc_action_points = (int, dex) => Math.floor((int + dex) / 12) + 1
+const calc_damage_modifier = (str, siz) => {
+    let total = str + siz
+    let modifier = ""
+
+    if (in_numeric_range(total, 0, 5)) {
+        modifier = "-1D8"
+    }
+    else if (in_numeric_range(total, 6, 10)) {
+        modifier = "-1D6"
+    }
+    else if (in_numeric_range(total, 11, 15)) {
+        modifier = "-1D4"
+    }
+    else if (in_numeric_range(total, 16, 20)) {
+        modifier = "-1D2"
+    }
+    else if (in_numeric_range(total, 21, 25)) {
+        modifier = "+0"
+    }
+    else if (in_numeric_range(total, 26, 30)) {
+        modifier = "+1D2"
+    }
+    else if (in_numeric_range(total, 31, 35)) {
+        modifier = "+1D4"
+    }
+    else if (in_numeric_range(total, 36, 40)) {
+        modifier = "+1D6"
+    }
+    else {
+        modifier = "{special}"
+    }
+
+    return modifier
+}
+const calc_experience_modifier = (cha) => Math.floor(cha / 6) - 1
+const calc_healing_rate = (con) => Math.floor(con / 6) + 1
+const calc_initiative_bonus = (int, dex) => (int + dex) / 2
+const calc_magic_points = (pow) => pow
+const calc_luck_points = (pow) => Math.floor(pow / 6) + 1
 
 const pool_2D6 = make_pool([D6, D6])
 const pool_3D6 = make_pool([D6, D6, D6])
@@ -57,7 +99,7 @@ let attributes = {
     },
     secondary: {
         action_points: 0,
-        damage_modifier: 0,
+        damage_modifier: "",
         experience_modifier: 0,
         healing_rate: 0,
         initiative_bonus: 0,
@@ -75,6 +117,14 @@ Object.keys(attribute_keys.primary).forEach(
 // TODO: move modifier back into assignment (roll) logic above... ?
 attributes.primary.size += 6
 attributes.primary.intelligence += 6
+
+attributes.secondary.action_points = calc_action_points(attributes.primary.intelligence, attributes.primary.dexterity)
+attributes.secondary.damage_modifier = calc_damage_modifier(attributes.primary.strength, attributes.primary.size)
+attributes.secondary.experience_modifier = calc_experience_modifier(attributes.primary.charisma)
+attributes.secondary.healing_rate = calc_healing_rate(attributes.primary.constitution)
+attributes.secondary.initiative_bonus = calc_initiative_bonus(attributes.primary.intelligence, attributes.primary.dexterity)
+attributes.secondary.magic_power_points = calc_magic_points(attributes.primary.power)
+attributes.secondary.luck_points = calc_luck_points(attributes.primary.power)
 
 // ui elements
 
